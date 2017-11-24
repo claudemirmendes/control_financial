070b8f7de19ffb731d843956ddb6e8aaee4fed82
claudemir mendes <avelinovqc@hotmail.com>
2017-11-24 20:08:33 -0300

    adjustment in routes and minor in auth_token.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   namespace :private do
     resources :account_payables
     resources :account_receivables
+    resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+
+  namespace :public do
+    resources :users do
+      collection do
+        put :login, on: collection
+      end
+    end
+ end
 end
