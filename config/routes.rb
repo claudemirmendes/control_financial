6d1a152f2d814c03bcbaa96d5caee45d0d2ecb09
claudemir mendes <avelinovqc@hotmail.com>
2017-11-24 17:51:23 -0300

    namespace private in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :account_payables
-  resources :account_receivables
+  namespace :private do
+    resources :account_payables
+    resources :account_receivables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
